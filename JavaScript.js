c5601de57574dd88c772db1055a122f2a95f9901
Kayden <90934152+tdkha@users.noreply.github.com>
2021-12-01 14:27:01 +0200

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -6,12 +6,49 @@ function clearScreen() {
    // This function displays values
 function display(value) {
     document.getElementById("result").value += value;
+
 }
    // This function evaluates the expression and return result
 function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
     document.getElementById("result").value = q;
+    document.getElementById("result2").value = q;
    }
 
+function ClearScreen2() {
+    document.getElementById("result2").value = "";
+   }
+function Evaluator(){
+    var q = document.getElementById("result").value;
+    var p = parseInt(q)
+    if (Math.sign(q) == 1){
+        document.getElementById("result2").value = "This is a positive number";
+    }else if (Math.sign(q) == -1){
+        document.getElementById("result2").value = "This is a negative number";
+    }else if (Math.sign(q) == 0){
+        document.getElementById("result2").value = "This is zero";
+    }else{
+        document.getElementById("result2").value = "Error!!!";
+    }
+}
+   
+function loopTask1(){
+    let userIntput = prompt("Enter words:")
+    let myString = "";
+    for (let i =0; i <userIntput.length; i ++){
+        myString += i + "." + userIntput + "\n";
+    }
+    alert(myString);
+} 
 
+function factorialize() {
+    let num = prompt("Enter words:");
+    if (num < 0) 
+          alert(-1);
+    else if (num == 0) 
+        alert (1);
+    else {
+        alert( (num * factorialize(num - 1)));
+    }
+  }
